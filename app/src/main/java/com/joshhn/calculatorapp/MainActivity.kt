a4a5fe97331abba7770c629a24ee5f43b4eba092
joshhn <duy.huynhngoc.05@gmail.com>
2022-06-21 12:45:48 -0400

    Upload all the files

---
+++ app/src/main/java/com/joshhn/calculatorapp/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.joshhn.calculatorapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private var tvInput: TextView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        tvInput = findViewById(R.id.tvInput)
+    }
+
+    fun onDigit(view: View){
+        tvInput?.append((view as Button).text.toString())
+    }
+
+    fun onOperand(view: View){
+
+    }
+
+    fun onCLR(view: View){
+
+    }
+}
